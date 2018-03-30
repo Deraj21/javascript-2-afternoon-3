882d9b856dbc358a8c9fefc468b02f7dcf49742e
deraj21 <deraj21@gmail.com>
2018-03-29 20:05:09 -0700

    All Done!

--- practice.js
+++ practice.js
@@ -69,7 +69,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -88,7 +90,9 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(arr, name, cb){
+  cb(arr.indexOf(name) !== -1);
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -109,7 +113,16 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+function uniq(arr, cb){
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++){
+      if ((i !== j) && (arr[i] === arr[j])){ // as long as not looking at same index, check to see if repeating
+        arr.splice(i, 1);
+      }
+    }
+  }
+  cb(arr);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -126,7 +139,9 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each(names, cb){
+  names.map((name, index) => cb(name, index));
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -143,7 +158,9 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+function getUserById(users, id, cb){
+  cb((users.filter(user => user.id === id))[0]);
+}
 
 // Do not edit the code below.
 var users = [
